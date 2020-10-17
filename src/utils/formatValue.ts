169d7f7882777c79f48ef28038c37b0fac14135e
ricardocaiuba <ricardo.caiuba@gmail.com>
2020-10-17 15:02:17 -0300

    Desafio fundamentos reactjs concluido

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,7 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('bt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
 
 export default formatValue;
